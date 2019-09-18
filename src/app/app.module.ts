ac7f4e6db82cd95f839ca4e52ca5cc13b77a7724
KhallilB <khallilbailey@gmail.com>
2019-09-18 10:39:19 -0700

    [add] Recipe Item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { RecipeComponent } from './components/recipe/recipe.component';
+import { RecipeItemComponent } from './components/recipe/recipe-item/recipe-item.component';
 import { RecipeListComponent } from './components/recipe/recipe-list/recipe-list.component';
 import { RecipeDetailsComponent } from './components/recipe/recipe-details/recipe-details.component';
 import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
-import { RecipeComponent } from './components/recipe/recipe.component';
 
 @NgModule({
   declarations: [
@@ -15,10 +16,11 @@ import { RecipeComponent } from './components/recipe/recipe.component';
     RecipeListComponent,
     RecipeDetailsComponent,
     ShoppingListComponent,
-    RecipeComponent
+    RecipeComponent,
+    RecipeItemComponent,
   ],
   imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
