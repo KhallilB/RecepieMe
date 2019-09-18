ef96ca539ae27fa6e415f5ac5b2fcdb7fa231750
KhallilB <khallil.bailey@gmail.com>
2019-09-17 21:58:19 -0700

    [add] recipe & shopping list components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { RecipeListComponent } from './components/recipe/recipe-list/recipe-list.component';
 import { RecipeDetailsComponent } from './components/recipe/recipe-details/recipe-details.component';
+import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
+import { RecipeComponent } from './components/recipe/recipe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     RecipeListComponent,
-    RecipeDetailsComponent
+    RecipeDetailsComponent,
+    ShoppingListComponent,
+    RecipeComponent
   ],
   imports: [BrowserModule],
   providers: [],
